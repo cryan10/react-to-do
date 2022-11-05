113e023c8566c756f0dc2197c51cd260a312a40d
Carrie <cryan1205@gmail.com>
2022-11-04 21:58:59 -0400

    feat(form): toggles form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { useState } from 'react';
 import AddTask from './components/AddTask';
 
 function App() {
+	const [showAddTask, setShowAddTask] = useState(false);
+
 	const [tasks, setTasks] = useState([
 		{
 			id: 1,
@@ -29,7 +31,7 @@ function App() {
 		const id = Math.floor(Math.random() * 10000) + 1;
 		const newTask = { id, ...task };
 		setTasks([...tasks, newTask]);
-	}
+	};
 
 	const deleteTask = (id) => {
 		setTasks(tasks.filter((task) => task.id !== id));
@@ -37,17 +39,19 @@ function App() {
 
 	const toggleReminder = (id) => {
 		setTasks(
-			tasks.map((task) => task.id === id
-				? { ...task, reminder: !task.reminder }
-				: task
+			tasks.map((task) =>
+				task.id === id ? { ...task, reminder: !task.reminder } : task
 			)
 		);
 	};
 
 	return (
 		<div className="container">
-			<Header />
-			<AddTask onAdd={addTask} />
+			<Header 
+			onAdd={() => setShowAddTask(!showAddTask)} 
+			showAdd = {showAddTask}
+			/>
+			{showAddTask && <AddTask onAdd={addTask} />}
 			{tasks.length > 0 ? (
 				<Tasks
 					tasks={tasks}
